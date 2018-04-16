e90cdf70977dee5e374f91258b7c5b393eba2937
mfarre1 <mfarre1@H1103.insmollerussa.edu>
2018-04-16 09:51:06 +0200

    Versió 0.2 del projecte m8daw2molle2

--- m8daw2molle2/src/m8daw2molle2/M8daw2molle2.java
+++ m8daw2molle2/src/m8daw2molle2/M8daw2molle2.java
@@ -29,6 +29,8 @@ public class M8daw2molle2 {
             bw.write(" </title>");bw.newLine();
             bw.write(" </head>");bw.newLine();
             bw.write(" <body>");bw.newLine();
+            bw.write(" <h1>DAW2 Pràctica 2: Github</h1>");
+            bw.newLine();
             bw.write(" Nova p&agrave;gina web");bw.newLine();
             bw.write(" </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
